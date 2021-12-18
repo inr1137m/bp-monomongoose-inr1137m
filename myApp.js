e1e7fa988ccbf5ba64cee2022db620d6fc2eeff2
Iyyanar M <30731340+inr1137m@users.noreply.github.com>
2021-12-18 03:14:40 +0000

    used findByIdAndRemove method to delete a recored by ID

--- myApp.js
+++ myApp.js
@@ -80,7 +80,11 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove({_id:personId},(err,data) => {
+    if(err) { console.log(err);} else{
+      done(null, data);
+    }
+  })
 };
 
 const removeManyPeople = (done) => {
